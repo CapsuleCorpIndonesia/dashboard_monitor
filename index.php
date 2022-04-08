5cc5dd133319c18e0fc1eeed33fb5627e2cdc525
kevkev013 <kevinkusnanto@yahoo.com>
2022-04-08 14:40:29 +0700

    tambah newman

--- index.php
+++ index.php
@@ -1,11 +1,13 @@
 <?php
 
-	$file = "/Users/kevinmac/Downloads/newman.txt";
-        $current = file_get_contents($file); 
+	// $file = "/newman.txt";
+ //        $current = file_get_contents($file); 
 
-        $info = date("Y-m-d H:i:s", filectime($file));
-        echo '<pre>';
-        print_r($info);
-        print_r($current);
-        echo '</pre>';
+ //        $info = date("Y-m-d H:i:s", filectime($file));
+ //        echo '<pre>';
+ //        print_r($info);
+ //        print_r($current);
+ //        echo '</pre>';
+
+	include('./newman/QATest-2022-04-08-07-26-23-254-0.html');
 ?>
