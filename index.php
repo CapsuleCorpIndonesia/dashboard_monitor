dc9a9b57fbac5de96f9e56e739ca3ae1f6b27e86
kevkev013 <kevinkusnanto@yahoo.com>
2022-04-08 15:12:45 +0700

    a

--- index.php
+++ index.php
@@ -3,11 +3,22 @@
 	// $file = "/newman.txt";
  //        $current = file_get_contents($file); 
 
- //        $info = date("Y-m-d H:i:s", filectime($file));
+        // $info = date("Y-m-d H:i:s", filectime($file));
  //        echo '<pre>';
  //        print_r($info);
  //        print_r($current);
  //        echo '</pre>';
+		// $filesInFolder = array();
+  //       $directory = "newman";
+  //       $iterator = new FilesystemIterator($directory);
 
-	include('./newman/QATest-2022-04-08-07-26-23-254-0.html');
+  //       foreach($iterator as $entry){
+  //           $filesInFolder[] = $entry->getFilename();
+  //       }
+
+  //       foreach($filesInFolder as $file){
+  //       	$info = date("Y-m-d H:i:s", filectime("./newman/".$file));
+  //           echo "<a href='./newman/$file'> $file </a>" . " | " . $info ."<br>";
+  //       }
+	include('./newman/Checkout Product-2022-04-08-08-10-41-942-0.html');
 ?>
